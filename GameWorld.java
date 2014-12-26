09d2cb1fe56681dad608c1498a93072d102985c7
Soeren Wegener <wegener92@gmail.com>
2014-12-26 19:02:58 +0100

    Implemented movement of snake.

--- GameWorld.java
+++ GameWorld.java
@@ -1,8 +1,16 @@
-class GameWorld {
+class GameWorld { //<>// //<>//
 
   GameTile[] gameTiles;
   int width;
   int height;
+  int snakeX;
+  int snakeY;
+  int snakeDirection;
+  int snakeLength;
+  static final int UP = 0;
+  static final int RIGHT = 1;
+  static final int DOWN = 2;
+  static final int LEFT = 3;
   static final int INITIAL_SNAKE_LENGTH = 5;
 
   GameWorld(int width, int height) {
@@ -11,7 +19,7 @@ class GameWorld {
     int worldArea = width * height;
     int tileArea = GameTile.TILE_SIZE * GameTile.TILE_SIZE;
     int numberOfTiles = worldArea / tileArea;
-    this.gameTiles = new GameTile[numberOfTiles]; //<>//
+    this.gameTiles = new GameTile[numberOfTiles];
     for (int i=0; i<numberOfTiles; i++) {
       int x = i % this.width;
       int y = i / this.width;
@@ -19,18 +27,36 @@ class GameWorld {
     }
     this.respawn();
   }
-  
+
   void draw(Snake mainClass) {
     for (GameTile tile : this.gameTiles) {
       tile.draw(mainClass);
     }
   }
-  
+
   void tick() {
+    switch(this.snakeDirection) {
+    case UP:
+      this.snakeY--;
+      break;
+    case RIGHT:
+      this.snakeX++;
+      break;
+    case DOWN:
+      this.snakeY++;
+      break;
+    case LEFT:
+      this.snakeY--;
+      break;
+    }
     for (GameTile tile : this.gameTiles) {
       tile.tick();
+      if (tile.x == this.snakeX && tile.y == this.snakeY) {
+        tile.occupied = true;
+        tile.occupiedCounter = this.snakeLength;
+      }
     }
-  } //<>//
+  }
 
   void respawn() {
     this.respawn(true);
@@ -38,7 +64,7 @@ class GameWorld {
 
   void respawn(boolean withWalls) {
     int i = 0;
-    int occupiedCounter = 1;
+    this.snakeLength = 0;
     for (GameTile tile : this.gameTiles) {
       int rightBorder = this.width - 1;
       int bottomBorder = this.height - 1;
@@ -49,10 +75,16 @@ class GameWorld {
       // startposition of snake in the middle of the world
       if (tile.x > this.width / 2 - INITIAL_SNAKE_LENGTH - 1 && tile.x < this.width / 2 && tile.y == this.height / 2) {
         tile.occupied = true;
-        tile.occupiedCounter = occupiedCounter;
-        occupiedCounter++;
+        this.snakeLength++;
+        tile.occupiedCounter = snakeLength;
+        // reached the head of the snake; save this position
+        if (snakeLength == INITIAL_SNAKE_LENGTH) {
+          this.snakeX = tile.x;
+          this.snakeY = tile.y;
+          this.snakeDirection = RIGHT;
+        }
+        i++;
       }
-      i++;
     }
   }
 }

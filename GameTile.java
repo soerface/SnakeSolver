df94a50f56c3001000d12bbaaac1ceb07aafaa75
Soeren Wegener <wegener92@gmail.com>
2014-12-26 17:50:29 +0100

    Draw tiles with random color for testing. Looks funny :) http://imgur.com/klP79CR

--- GameTile.java
+++ GameTile.java
@@ -10,5 +10,11 @@ class GameTile {
   }
 
   void draw(Snake mainClass) {
+    float r = mainClass.random(0, 255);
+    float g = mainClass.random(0, 255);
+    float b = mainClass.random(0, 255);
+    mainClass.fill(r, g, b);
+    mainClass.stroke(0xffaaaaaa);
+    mainClass.rect(this.x * TILE_SIZE, this.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
   }
 }

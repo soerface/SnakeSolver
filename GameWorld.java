5da6f21fa913975a166da938f7f49a5035ebee5d
Soeren Wegener <wegener92@gmail.com>
2014-12-26 18:26:04 +0100

    Moved setup_walls to GameWorld class and added attributes width and height to GameWorld.

--- GameWorld.java
+++ GameWorld.java
@@ -1,17 +1,22 @@
 class GameWorld {
 
   GameTile[] gameTiles;
+  int width;
+  int height;
 
   GameWorld(int width, int height) {
+    this.width = width / GameTile.TILE_SIZE;
+    this.height = height / GameTile.TILE_SIZE;
     int worldArea = width * height;
     int tileArea = GameTile.TILE_SIZE * GameTile.TILE_SIZE;
     int numberOfTiles = worldArea / tileArea;
     this.gameTiles = new GameTile[numberOfTiles]; //<>//
     for (int i=0; i<numberOfTiles; i++) {
-      int x = i % (width / GameTile.TILE_SIZE);
-      int y = i / (width / GameTile.TILE_SIZE);
+      int x = i % this.width;
+      int y = i / this.width;
       this.gameTiles[i] = new GameTile(x, y);
     }
+    this.setup_walls();
   }
   
   void draw(Snake mainClass) {
@@ -25,4 +30,14 @@ class GameWorld {
       tile.tick();
     }
   } //<>//
+
+  void setup_walls() {
+    for (GameTile tile : this.gameTiles) {
+      int rightBorder = this.width - 1;
+      int bottomBorder = this.height - 1;
+      if (tile.x == 0 || tile.y == 0 || tile.x == rightBorder || tile.y == bottomBorder) {
+        tile.occupied = true;
+      }
+    }
+  }
 }
--- Snake.pde
+++ Snake.pde
@@ -3,17 +3,6 @@ GameWorld gameWorld;
 void setup() {
   size(600, 500);
   this.gameWorld = new GameWorld(width, height);
-  setup_walls();
-}
-
-void setup_walls() {
-  for (GameTile tile : this.gameWorld.gameTiles) {
-    int rightBorder = width / tile.TILE_SIZE - 1;
-    int bottomBorder = height / tile.TILE_SIZE - 1;
-    if (tile.x == 0 || tile.y == 0 || tile.x == rightBorder || tile.y == bottomBorder) {
-      tile.occupied = true;
-    }
-  }  
 }
 
 void draw() {

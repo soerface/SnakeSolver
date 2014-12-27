b45a78b0acb51a0337aeeeebfef931363ea0cbe8
Soeren Wegener <wegener92@gmail.com>
2014-12-27 05:01:51 +0100

    Added method to get free neighbour tiles.

--- AutoSolver.java
+++ AutoSolver.java
@@ -23,12 +23,44 @@ class AutoSolver {
     int y = this.gameWorld.snakeY;
     int tileId = this.getTileId(x, y);
     this.openList.add(new Node(tileId));
+    int neighborTileIds[] = this.getNeighbourTileIds(x, y);
   }
 
   int getTileId(int x, int y) {
     return x + y * this.gameWorld.width;
   }
   
+  int[] getNeighbourTileIds(int x, int y) {
+    int[] potentialNeighbours = new int[]{-1, -1, -1, -1};
+    if (x > 0) {
+      potentialNeighbours[0] = getTileId(x-1, y);
+    }
+    if (y > 0) {
+      potentialNeighbours[1] = getTileId(x, y-1);
+    }
+    if (x < this.gameWorld.width) {
+      potentialNeighbours[2] = getTileId(x+1, y);
+    }
+    if (y < this.gameWorld.height) {
+      potentialNeighbours[3] = getTileId(x, y+1);
+    }
+    int totalNeighbours = 0;
+    for (int n : potentialNeighbours) {
+      if (n > -1 && !this.gameWorld.gameTiles[n].occupied) {
+        totalNeighbours++;
+      }
+    }
+    int[] neighbourTiles = new int[totalNeighbours];
+    int i = 0;
+    for (int n : potentialNeighbours) {
+      if (n > -1 && !this.gameWorld.gameTiles[n].occupied) {
+        neighbourTiles[i] = n;
+        i++;
+      }
+    }
+    return neighbourTiles;
+  }
+
   void draw() {
   }
 
